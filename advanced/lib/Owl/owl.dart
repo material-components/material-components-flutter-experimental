266945402e26fe238ed65cd52e041ad452c55cd3
Will Larche <larche@google.com>
2018-10-25 03:16:47 -0400

    [Owl] Making it scroll instead of overflow.

--- advanced/lib/Owl/owl.dart
+++ advanced/lib/Owl/owl.dart
@@ -71,26 +71,28 @@ class _OwlState extends State<OwlWidget> with SingleTickerProviderStateMixin {
         backgroundColor: Colors.white,
         body: Stack(
           children: <Widget>[
-            Column(children: <Widget>[
-              Image(image: AssetImage('assets/1.png')),
-              SizedBox(height: 15.0),
-              Text('TRAVEL', style: textThemeOwl.subhead),
-              SizedBox(height: 5.0),
-              Text(
-                '\'Glamping\'\nand Other Annoying Portmanteaus',
-                style: textThemeOwl.display1,
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 15.0),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'To access the tutorial videos for a course, tap the curved shape in the bottom right of the screen. This section is a hub for course content. Each course has an overview screen that contains tutorial videos.\nOwl is an educational app that provides courses for people who want to explore and learn new skills in design, art, architecture, and fashion. The Owl brand uses bold color, shape, and typography to express its brand attributes: energy, daring, and fun.\nOwl’s design reflects the energy and excitement of learning a new skill.',
-                  style: textThemeOwl.body1,
+            ListView(
+              children: [Column(children: <Widget>[
+                Image(image: AssetImage('assets/1.png')),
+                SizedBox(height: 15.0),
+                Text('TRAVEL', style: textThemeOwl.subhead),
+                SizedBox(height: 5.0),
+                Text(
+                  '\'Glamping\'\nand Other Annoying Portmanteaus',
+                  style: textThemeOwl.display1,
                   textAlign: TextAlign.center,
                 ),
-              ),
-            ]),
+                SizedBox(height: 15.0),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'To access the tutorial videos for a course, tap the curved shape in the bottom right of the screen. This section is a hub for course content. Each course has an overview screen that contains tutorial videos.\nOwl is an educational app that provides courses for people who want to explore and learn new skills in design, art, architecture, and fashion. The Owl brand uses bold color, shape, and typography to express its brand attributes: energy, daring, and fun.\nOwl’s design reflects the energy and excitement of learning a new skill.',
+                    style: textThemeOwl.body1,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ])],
+            ),
             SafeArea(
               child: Column(children: <Widget>[
                 IconButton(
