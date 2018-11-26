0d47dc69f9fff88051c4dad60a193d6975b61ea9
Anthony Robledo <anthonyrobledo@gmail.com>
2018-11-26 13:09:59 -0500

    smooth points and controls for demo

--- rally_proto/lib/charts/rally_line_chart.dart
+++ rally_proto/lib/charts/rally_line_chart.dart
@@ -21,17 +21,43 @@ class RallyLineChartPainter extends CustomPainter {
   RallyLineChartPainter(this.context, this.events);
 
   final BuildContext context;
+
+  // Events to plot on the line as points.
   final List<DetailedEventItem> events;
 
-  final double space = 16.0;
+  // Number of days to plot.
+  // This is hardcoded to reflect the dummy data, but would be dynamic in a real app.
+  // For demo only.
   final int numDays = 52;
-  final int shift = 3;
+
+  // Beginning of window. The end is this plus numDays.
+  // This is hardcoded to reflect the dummy data, but would be dynamic in a real app.
+  // For demo only.
+  final DateTime startDate = DateTime.utc(2018, 12, 1);
+
+  // Ranges uses to lerp the pixel points.
+  // This is hardcoded to reflect the dummy data, but would be dynamic in a real app.
+  // For demo only.
+  final double maxAmount = 3000.0; // minAmount is assumed to be 0.0
+
+  // The number of milliseconds in a day. This is the inherit period fot the points in this line.
+  final int millisInDay = 24 * 60 * 60 * 1000;
+
+  // Amount to shift the tick drawing by so that the sunday ticks do not start on the edge.
+  // For demo only.
+  final int tickShift = 3;
+
+  // Arbitrary unit of space for absolute positioned painting.
+  final double space = 16.0;
+
 
   @override
   void paint(Canvas canvas, Size size) {
-    _drawXAxisLabels(canvas, size);
-    _drawXAxisTicks(canvas, size.width, size.height - space);
-    _drawLine(canvas, size);
+    double ticksTop = size.height - space * 5.0;
+    double labelsTop = size.height - space * 2;
+    _drawLine(canvas, Rect.fromLTWH(0.0, 0.0, size.width, ticksTop));
+    _drawXAxisTicks(canvas, Rect.fromLTWH(0.0, ticksTop, size.width, labelsTop - ticksTop));
+    _drawXAxisLabels(canvas, Rect.fromLTWH(0.0, labelsTop, size.width, size.height - labelsTop));
   }
 
   @override
@@ -39,7 +65,63 @@ class RallyLineChartPainter extends CustomPainter {
     return false;
   }
 
-  _drawXAxisLabels(Canvas canvas, Size size) {
+  _drawLine(Canvas canvas, Rect rect) {
+    Paint linePaint = Paint()
+      // TODO(demo): Change the line color here. Try passing 0, 1, 3, etc.
+      ..color = RallyColors.getAccountColor(1)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0;
+
+    // Arbitrary value for the first point. In a real app, a wider range of
+    // points would be used that go beyond the boundaries of the screen.
+    double lastAmount = 800.0;
+
+    // TODO(demo): This is the number of points to skip. The higher it is, the more smooth curve will be when drawing quadratic splines.
+    int smooth = 4; // try 1, 7, 15, etc.
+
+    // Align the points with equal deltas (1 day) as a cumulative sum.
+    int startMillis = startDate.millisecondsSinceEpoch;
+    final List<Offset> points = [Offset(0, (maxAmount - lastAmount) / maxAmount * rect.height)];
+    for (int i = 0; i < numDays + smooth; i++) {
+      int endMillis = startMillis + millisInDay * 1;
+      List<DetailedEventItem> filteredEvents = events.where((e) => startMillis <= e.date.millisecondsSinceEpoch && e.date.millisecondsSinceEpoch <= endMillis).toList();
+      lastAmount += filteredEvents.fold(0, (sum, e) => sum + e.amount);
+      double x = i / numDays * rect.width;
+      double y = (maxAmount - lastAmount) / maxAmount * rect.height;
+      points.add(Offset(x, y));
+      startMillis = endMillis;
+    }
+
+    final Path path = Path();
+    path.moveTo(points[0].dx, points[0].dy);
+    for (int i = 1; i < points.length - smooth; i += smooth) {
+      double x1 = points[i].dx;
+      double y1 = points[i].dy;
+
+      // TODO(demo): Comment out the next line and uncomment the next 3 to draw smoother lines between points.
+            path.lineTo(x1, y1);
+//      double x2 = (x1 + points[i + smooth].dx) / 2;
+//      double y2 = (y1 + points[i + smooth].dy) / 2;
+//      path.quadraticBezierTo(x1, y1, x2, y2);
+    }
+    canvas.drawPath(path, linePaint);
+  }
+
+  _drawXAxisTicks(Canvas canvas, Rect rect) {
+    double dayTop = (rect.top + rect.bottom) / 2;
+    for (int i = 0; i < numDays; i++) {
+      double x =  rect.width / numDays * i;
+      canvas.drawRect(
+          new Rect.fromPoints(Offset(x, i % 7 == tickShift ? rect.top : dayTop), Offset(x, rect.bottom)),
+          new Paint()
+            ..style = PaintingStyle.stroke
+            ..strokeWidth = 1.0
+            ..color = RallyColors.gray25a
+      );
+    }
+  }
+
+  _drawXAxisLabels(Canvas canvas, Rect rect) {
     TextPainter leftLabel = TextPainter(
       text: TextSpan(
           text: 'DEC 2018',
@@ -48,7 +130,7 @@ class RallyLineChartPainter extends CustomPainter {
       textDirection: TextDirection.ltr,
     );
     leftLabel.layout();
-    leftLabel.paint(canvas, Offset(0.0 + space / 2, size.height - leftLabel.height - space));
+    leftLabel.paint(canvas, Offset(rect.left + space / 2, rect.center.dy));
 
     TextPainter centerLabel = TextPainter(
       text: TextSpan(
@@ -58,7 +140,7 @@ class RallyLineChartPainter extends CustomPainter {
       textDirection: TextDirection.ltr,
     );
     centerLabel.layout();
-    centerLabel.paint(canvas, Offset((size.width - centerLabel.width) / 2, size.height - centerLabel.height - space));
+    centerLabel.paint(canvas, Offset((rect.width - centerLabel.width) / 2, rect.center.dy));
 
     TextPainter rightLabel = TextPainter(
       text: TextSpan(
@@ -68,87 +150,6 @@ class RallyLineChartPainter extends CustomPainter {
       textDirection: TextDirection.ltr,
     );
     rightLabel.layout();
-    rightLabel.paint(canvas, Offset(size.width - centerLabel.width - space / 2, size.height - rightLabel.height - space));
-  }
-
-  _drawXAxisTicks(Canvas canvas, double width, double bottomOffset) {
-    double top = bottomOffset - space * 3.5;
-    double weekTop = bottomOffset - space * 5.0;
-    double bottom = bottomOffset - space * 2;
-    for (int i = 0; i < numDays; i++) {
-      double x =  width / numDays * i;
-      canvas.drawRect(
-          new Rect.fromPoints(Offset(x, i % 7 == shift ? weekTop : top), Offset(x, bottom)),
-          new Paint()
-            ..style = PaintingStyle.stroke
-            ..strokeWidth = 1.0
-            ..color = RallyColors.gray25a
-      );
-    }
-  }
-
-  _drawLine(Canvas canvas, Size sizgie) {
-    Paint linePaint = Paint()
-      ..color = RallyColors.getAccountColor(1)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 2.0;
-
-    // The top left of the window is at 0,0, so these are the same as right and bottom.
-    double windowWidth = size.width;
-    double windowHeight = size.height - space * 5.0;
-
-    // Ranges uses to lerp the pixel points.
-    int millisInDay = 24 * 60 * 60 * 1000;
-    int windowMillis = numDays * millisInDay;
-    double maxAmount = 3000.0; // minAmount is 0.0
-
-    // Beginning of window. The end is this plus numDays.
-    DateTime startDate = DateTime.utc(2018, 12, 1);
-
-    // Arbitrary value for the first point. In a real app, a wider range of
-    // points would be used that go beyond the boudnaries of the screen.
-    double lastAmount = 500.0;
-
-    int startMillis = startDate.millisecondsSinceEpoch;
-    final List<Offset> points = [Offset(0, (maxAmount - lastAmount) / maxAmount * windowHeight)];
-    for (int i = 0; i < numDays; i += 1) {
-      int endMillis = startMillis + millisInDay * 1;
-      List<DetailedEventItem> filteredEvents = events.where((e) => startMillis <= e.date.millisecondsSinceEpoch && e.date.millisecondsSinceEpoch <= endMillis).toList();
-      lastAmount += filteredEvents.fold(0, (sum, e) => sum + e.amount);
-      print(lastAmount);
-      double x = i / numDays * windowWidth;
-      double y = (maxAmount - lastAmount) / maxAmount * windowHeight;
-      points.add(Offset(x, y));
-      startMillis = endMillis;
-    }
-
-    // Create a list of points in terms of pixels from top left.
-    // TODO(clocksmith): Align the points for smooth curves.
-//    final List<Offset> points = [Offset(0, (maxAmount - lastAmount) / maxAmount * windowHeight)];
-//    points.addAll(events.reversed.map((event) {
-//      lastAmount += event.amount;
-//      int diffMiliis = event.date.millisecondsSinceEpoch - startDate.millisecondsSinceEpoch;
-//      double x = diffMiliis / windowMillis * windowWidth;
-//      double y = (maxAmount - lastAmount) / maxAmount * windowHeight;
-//      return Offset(x, y);
-//    }));
-
-
-    final Path path = Path();
-    path.moveTo(points[0].dx, points[0].dy);
-    for (int i = 0; i < points.length - 1; i += 8) {
-      double x1 = (points[i].dx + points[i + 1].dx) / 2;
-      double y1 = (points[i].dy + points[i + 1].dy) / 2;
-      double x2 = (x1 + points[i + 1].dx) / 2;
-      double y2 = (y1 + points[i + 1].dy) / 2;
-      double x3 = points[i + 1].dx;
-      double y3 = points[i + 1].dy;
-//      path.conicTo(x1, y1, x3, y3, 1);
-      path.cubicTo(x1, y1, x2, y2, x3, y3);
-    }
-//    for (int i = 1; i < points.length; i += 2) {
-//      path.quadraticBezierTo(points[i - 1].dx, points[i - 1].dy, points[i].dx, points[i].dy);
-//    }
-    canvas.drawPath(path, linePaint);
+    rightLabel.paint(canvas, Offset(rect.right - centerLabel.width - space / 2, rect.center.dy));
   }
 }
