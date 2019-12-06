09bf6bd005a9b816092dc6fdc24cfe75ddb74c16
Anthony <anthonyrobledo@gmail.com>
2019-12-06 16:40:03 -0500

    Google fonts text theme (#47)
    * Add TextTheme generated methods with tests

--- google_fonts/test/generated_font_methods_test.dart
+++ google_fonts/test/generated_font_methods_test.dart
@@ -18,7 +18,8 @@ main() {
       fontStyle: FontStyle.normal,
     );
 
-    final outputTextStyle = GoogleFonts.roboto(textStyle: inputTextStyle);
+    final outputTextStyle =
+        GoogleFonts.robotoTextStyle(textStyle: inputTextStyle);
 
     expect(outputTextStyle.fontFamily, equals('Roboto_regular'));
   });
@@ -30,7 +31,8 @@ main() {
       fontStyle: FontStyle.italic,
     );
 
-    final outputTextStyle = GoogleFonts.roboto(textStyle: inputTextStyle);
+    final outputTextStyle =
+        GoogleFonts.robotoTextStyle(textStyle: inputTextStyle);
 
     expect(outputTextStyle.fontFamily, equals('Roboto_italic'));
   });
@@ -42,7 +44,8 @@ main() {
       fontStyle: FontStyle.normal,
     );
 
-    final outputTextStyle = GoogleFonts.roboto(textStyle: inputTextStyle);
+    final outputTextStyle =
+        GoogleFonts.robotoTextStyle(textStyle: inputTextStyle);
 
     expect(outputTextStyle.fontFamily, equals('Roboto_500'));
   });
@@ -54,7 +57,8 @@ main() {
       fontStyle: FontStyle.italic,
     );
 
-    final outputTextStyle = GoogleFonts.roboto(textStyle: inputTextStyle);
+    final outputTextStyle =
+        GoogleFonts.robotoTextStyle(textStyle: inputTextStyle);
 
     expect(outputTextStyle.fontFamily, equals('Roboto_500italic'));
   });
@@ -69,7 +73,8 @@ main() {
       fontStyle: FontStyle.italic,
     );
 
-    final outputTextStyle = GoogleFonts.cardo(textStyle: inputTextStyle);
+    final outputTextStyle =
+        GoogleFonts.cardoTextStyle(textStyle: inputTextStyle);
 
     expect(outputTextStyle.fontFamily, equals('Cardo_italic'));
   });
@@ -81,7 +86,7 @@ main() {
       fontStyle: FontStyle.italic,
     );
 
-    final outputTextStyle = GoogleFonts.cardo(
+    final outputTextStyle = GoogleFonts.cardoTextStyle(
       textStyle: inputTextStyle,
       fontWeight: FontWeight.w200,
       fontStyle: FontStyle.normal,
@@ -93,7 +98,7 @@ main() {
 
   testWidgets('Defaults to regular when no Text style is passed',
       (tester) async {
-    final outputTextStyle = GoogleFonts.lato();
+    final outputTextStyle = GoogleFonts.latoTextStyle();
 
     expect(outputTextStyle.fontFamily, equals('Lato_regular'));
   });
@@ -101,7 +106,7 @@ main() {
   testWidgets(
       'Defaults to regular when a Text style with no weight or style is passed',
       (tester) async {
-    final outputTextStyle = GoogleFonts.lato(textStyle: TextStyle());
+    final outputTextStyle = GoogleFonts.latoTextStyle(textStyle: TextStyle());
 
     expect(outputTextStyle.fontFamily, equals('Lato_regular'));
   });
@@ -109,14 +114,14 @@ main() {
   testWidgets('fontSize is honored when passed in via a TextStyle param',
       (tester) async {
     final textStyle = TextStyle(fontSize: 37);
-    final outputTextStyle = GoogleFonts.rancho(textStyle: textStyle);
+    final outputTextStyle = GoogleFonts.ranchoTextStyle(textStyle: textStyle);
 
     expect(outputTextStyle.fontSize, equals(37));
   });
 
   testWidgets('fontSize is honored from a passed in the fontSize param',
       (tester) async {
-    final outputTextStyle = GoogleFonts.rancho(fontSize: 31);
+    final outputTextStyle = GoogleFonts.ranchoTextStyle(fontSize: 31);
 
     expect(outputTextStyle.fontSize, equals(31));
   });
@@ -125,11 +130,51 @@ main() {
       'fontSize from top level fontSize param takes precedence over fontSize '
       'from TextStyle param', (tester) async {
     final textStyle = TextStyle(fontSize: 41);
-    final outputTextStyle = GoogleFonts.rancho(
+    final outputTextStyle = GoogleFonts.ranchoTextStyle(
       textStyle: textStyle,
       fontSize: 47,
     );
 
     expect(outputTextStyle.fontSize, equals(47));
+  });
+
+  testWidgets('TextTheme method works in the default case', (tester) async {
+    final textTheme = GoogleFonts.oswaldTextTheme();
+    final expectedFamilyWithVariant = 'Oswald_regular';
+
+    expect(textTheme.display4.fontFamily, equals(expectedFamilyWithVariant));
+    expect(textTheme.display3.fontFamily, equals(expectedFamilyWithVariant));
+    expect(textTheme.display2.fontFamily, equals(expectedFamilyWithVariant));
+    expect(textTheme.display1.fontFamily, equals(expectedFamilyWithVariant));
+    expect(textTheme.headline.fontFamily, equals(expectedFamilyWithVariant));
+    expect(textTheme.title.fontFamily, equals(expectedFamilyWithVariant));
+    expect(textTheme.subhead.fontFamily, equals(expectedFamilyWithVariant));
+    expect(textTheme.body2.fontFamily, equals(expectedFamilyWithVariant));
+    expect(textTheme.body1.fontFamily, equals(expectedFamilyWithVariant));
+    expect(textTheme.caption.fontFamily, equals(expectedFamilyWithVariant));
+    expect(textTheme.button.fontFamily, equals(expectedFamilyWithVariant));
+    expect(textTheme.subtitle.fontFamily, equals(expectedFamilyWithVariant));
+    expect(textTheme.overline.fontFamily, equals(expectedFamilyWithVariant));
+  });
+
+  testWidgets('TextTheme method works with a base textTheme', (tester) async {
+    // In app this is usually obtained by Theme.of(context).textTheme.
+    final baseTextTheme = TextTheme(
+      display2: TextStyle(fontWeight: FontWeight.w700),
+      body1: GoogleFonts.acmeTextStyle(),
+      subtitle: TextStyle(fontStyle: FontStyle.italic),
+    );
+
+    final textTheme = GoogleFonts.oswaldTextTheme(textTheme: baseTextTheme);
+    final expectedFamilyWithVariant = 'Oswald_regular';
+
+    // Default is preserved.
+    expect(textTheme.display1.fontFamily, equals(expectedFamilyWithVariant));
+    // Different font family gets overridden by oswald.
+    expect(textTheme.body1.fontFamily, equals(expectedFamilyWithVariant));
+    // Weight is preserved.
+    expect(textTheme.display2.fontWeight, equals(FontWeight.w700));
+    // Style is preserved.
+    expect(textTheme.subtitle.fontStyle, equals(FontStyle.italic));
   });
 }
