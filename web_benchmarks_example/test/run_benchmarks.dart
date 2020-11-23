24d798889db18bccffc7336829bd243193bda74f
Tianguang Zhang <penn.zht@gmail.com>
2020-11-23 11:45:32 +0100

    Update interface.

--- web_benchmarks_example/test/run_benchmarks.dart
+++ web_benchmarks_example/test/run_benchmarks.dart
@@ -1,10 +1,11 @@
 import 'dart:convert' show JsonEncoder;
+import 'dart:io';
 
 import 'package:web_benchmarks/server.dart';
 
 Future<void> main() async {
-  final taskResult = await runWebBenchmark(
-    macrobenchmarksDirectory: '.',
+  final taskResult = await serveWebBenchmark(
+    benchmarkAppDirectory: Directory('.'),
     entryPoint: 'lib/benchmarks/runner.dart',
     useCanvasKit: false,
   );
