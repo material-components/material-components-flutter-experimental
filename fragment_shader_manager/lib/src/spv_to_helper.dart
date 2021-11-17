6d9dc5b47c6d1ce20b4a259567d22a9698756833
Anthony <591699+clocksmith@users.noreply.github.com>
2021-11-17 12:07:31 -0500

    remove bazel in favor pf pub only

--- fragment_shader_manager/lib/src/spv_to_helper.dart
+++ fragment_shader_manager/lib/src/spv_to_helper.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 import 'package:args/args.dart';
 import 'package:logging/logging.dart';
 
-const inputFileOption = 'input_file';
-const outputFileOption = 'output_file';
+const inputFilePrefixOption = 'input_file_prefix';
+const outputFilePrefixOption = 'output_file_prefix';
 const formatWithNewlinesFlag = 'format_with_newlines';
 
 const space = ' ';
@@ -15,15 +15,17 @@ Future<void> main(List<String> arguments) async {
 
   // Parse the command line args for input and output paths.
   final parser = ArgParser()
-    ..addOption(inputFileOption, abbr: 'i')
-    ..addOption(outputFileOption, abbr: 'o')
+    ..addOption(inputFilePrefixOption, abbr: 'i')
+    ..addOption(outputFilePrefixOption, abbr: 'o')
     ..addFlag(formatWithNewlinesFlag, abbr: 'f', defaultsTo: false);
   final argResults = parser.parse(arguments);
 
-  // Read in the spirv binary as a list of bytes.
-  final inputFilePath = argResults[inputFileOption].toString();
-  log.info('Reading spirv bytes from $inputFilePath');
-  final bytes = await File(inputFilePath).readAsBytes();
+  // Read in the glsl as a string and spirv binary as a list of bytes.
+  final inputFilePrefix = argResults[inputFilePrefixOption].toString();
+  log.info('Reading GLSL string from $inputFilePrefix');
+  final glslString = await File('$inputFilePrefix.glsl').readAsString();
+  log.info('Reading SPIR-V bytes from $inputFilePrefix');
+  final spirvBytes = await File('$inputFilePrefix.spv').readAsBytes();
 
   final out = StringBuffer();
   out.write('''
@@ -31,13 +33,13 @@ Future<void> main(List<String> arguments) async {
 
 import 'dart:typed_data';
 
-/// A class for managing [FragmentShaderBuilder] that includes a pre-transpiled
+/// A class for managing [FragmentProgram] that includes a pre-transpiled
 /// shader program into SPIR-V.
 ///
 /// Words in SPIR-V are 32 bits. Every 4 elements in this list represents 1
 /// SPIR-V word. See https://www.khronos.org/registry/SPIR-V/.
 
-/// TODO(clocksmith): add original source glsl and/or spvasm as commented text.
+/// TODO(clocksmith): add original source glsl and/or spvasm as static strings and/or commented text.
 ''');
 
   // Create a const Uint8List that is created from the spirv bytes.
@@ -46,7 +48,7 @@ import 'dart:typed_data';
   var byteCount = 0;
   var newline = () { return byteCount % 4 == 0 && formatWithNewlines; };
   if (newline()) out.writeln();
-  for (final byte in bytes) {
+  for (final byte in spirvBytes) {
     if (newline()) out.write(twoSpaces);
     out.write('0x${byte.toRadixString(16).padLeft(2, '0').toUpperCase()},');
     byteCount++;
@@ -57,9 +59,9 @@ import 'dart:typed_data';
   // Create and save the output file. Existing files will not be overwritten,
   // but when running this script from a genrule, there will not be a different
   // existing file in the output directory.
-  final outputFilePath = argResults[outputFileOption].toString();
-  final outFile = File(outputFilePath);
-  await outFile.create(recursive: true);
+  final outputFilePrefix = argResults[outputFilePrefixOption].toString();
+  final outputFilePath = '$outputFilePrefix.g.dart';
+  final outFile = File(outputFilePath); await outFile.create(recursive: true);
   log.info('Writing spirv Uint8List to $outputFilePath');
   await outFile.writeAsString(out.toString());
 }
